ee58e56f01d07d12ba8792d01f16c36a52ba6ffc
Syndicate <92220126+Juffrii@users.noreply.github.com>
2024-04-04 18:33:43 +0300

    Delete ReactionModel.py

--- ReactionModel.py
+++
@@ -1,6 +0,0 @@
-class Reaction:
-    def __init__(self, compounds: str, result: str, ion: bool, is_char: bool):
-        self.compound = compounds
-        self.result = result
-        self.ion = ion
-        self.is_char = is_char
